60200d74b5b3e19cbb4c1019d238c3b2035306f6
Akash Jeganath <akashjoffl@icloud.com>
2019-12-16 18:46:12 +0530

    4.6 Tuples

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,8 +1,5 @@
-print("*** Exercise: List Method ****")
+print("*** Tuples ****")
 
-numbers = [3, 3, 5, 6, 7, 7, 8, 6, 9]
-uniques = []
-for number in numbers:
-    if number not in uniques:
-        uniques.append(number)
-print(uniques)
+numbers = (1, 2, 3)
+"numbers[0] = 20"
+print(numbers[0])
