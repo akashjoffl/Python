76c0a975074348c46341acdebc3e74fd528e94e2
Akash Jeganath <akashjoffl@icloud.com>
2019-12-16 18:58:16 +0530

    4.8 Dictionaries

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,9 +1,10 @@
-print("*** Unpacking ****")
+print("*** Dictionaries ****")
 
-coordinates = (1, 2, 3)
-
-x, y, z = coordinates
-
-print(y)
-print(z)
-
+customer = {
+    "name": "Akash Jeganath",
+    "age": 30,
+    "is_verified": True
+}
+customer["age"] = 20
+print(customer.get("dob", "4th of June 1999"))
+print(customer["age"])
