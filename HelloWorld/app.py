7db612333409f883728d84505e7b555e39f3340a
Akash Jeganath <akashjoffl@icloud.com>
2020-01-01 21:22:11 +0530

    Dictionaries (updated)

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,10 +1,24 @@
-print("*** Dictionaries ****")
+print("*** Exercise: Dictionaries ****")
 
-customer = {
-    "name": "Akash Jeganath",
-    "age": 30,
-    "is_verified": True
+phone = input("Phone: ")
+digits_mapping = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "10": "Ten",
 }
-customer["age"] = 20
-print(customer.get("dob", "4th of June 1999"))
-print(customer["age"])
+output = ""
+for key in phone:
+    output += digits_mapping.get(key, "!") + " "
+print(output)
+
+
+
+
