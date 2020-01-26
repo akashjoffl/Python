1b84d68ee8ef2c2602692f460e4f6c2746b5211a
Akash Jeganath <akashjoffl@icloud.com>
2020-01-26 23:57:06 +0530

    Return Statement

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -1,10 +1,6 @@
-print("****  Keyword Arguments  ****")
+print("****  Return Statement  ****")
 
-def greet_user(first_name, last_name):
-    print(f'Hi {first_name} {last_name}!')
-    print('Welcome aboard')
-
-# Best practise to keep 2 line breaks after defining functions in Python
-print("Start")
-greet_user(first_name = "Akash",last_name = "Jeganath")
-print("Finish")
+def square(number):
+    return number * number
+    
+print(square(3))   
